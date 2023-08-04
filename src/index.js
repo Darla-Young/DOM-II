a7f6eafd9a2bafad0dfd83b26299d642007c1b6d
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-08-04 13:40:38 -0500

    4 of 10 events complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import './less/index.less'
 
-// Your code goes here!
+// 1. mouseover &
+// 2. mouseout
+const nav = document.querySelector('.nav');
+nav.addEventListener('mouseover', event => {
+    event.target.style.color = 'gray';
+});
+nav.addEventListener('mouseout', event => {
+    event.target.style.color = 'black';
+});
+
+// 3. scroll &
+// 4. scrollend
+const home = document.querySelector('body');
+document.addEventListener('scroll', () => {
+    home.style.color = 'gray';
+});
+document.addEventListener('scrollend', () => {
+    setTimeout(() => {
+        home.style.color = 'black';
+    }, 0);
+});
+
+// 4. load
+
+// 5. focus
+
+// 6. resize
+
+// 7. scroll
+
+// 8. select
+
+// 9. dblclick
+
+// 10. drag / drop
